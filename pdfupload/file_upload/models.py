a6794338b8f681f693ffb1f48845ea5e39c6173d
saavrabh <saurabhbharswadkar7@gmail.com>
2016-08-27 13:23:17 +0530

    migration done after creating models

--- pdfupload/file_upload/models.py
+++ pdfupload/file_upload/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
 # Create your models here.
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=200)
+    Project_id =  models.IntegerField()
+    has_pdfs = []
+
+class Files(models.Model):
+    associate_project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    file_id = models.IntegerField()
+    has_projects = []
+  
--- pdfupload/file_upload/views.py
+++ pdfupload/file_upload/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+import models
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def get_files(request,proj_id):
+	return 1
+
+def add_files(request,proj_id):
+	
